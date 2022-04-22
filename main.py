72fbf09c9727856686adbdd720768920e2905cef
Emad Siddiqui <62686818+EmadSid14@users.noreply.github.com>
2022-04-22 15:56:03 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def main_apv():
     print (43*'~')
     main_input()
 def main_input():
-    mx=eval(input('\x1b[1;92m[!] Select : '))
+    mx=int(input('\x1b[1;92m[!] Select : '))
     print ("")
     if mx=='1':
         print ("")
@@ -142,7 +142,7 @@ def main_input():
             main()
     elif mx=='4':
         print ("")
-        print('\033[1;94m Cheking Subscription ....\033[1;97m')
+        print('\033[1;94m Checking Subscription ....\033[1;97m')
         time.sleep(3)
         grap()
     elif mx=='5':
