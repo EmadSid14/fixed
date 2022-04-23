d74bcad3100187ababfb8459b6d30d0cae2eab27
Emad Siddiqui <62686818+EmadSid14@users.noreply.github.com>
2022-04-23 05:55:32 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -278,9 +278,9 @@ def fb_menu():
             print (logo)
             print ("\033[1;91m\n[*] Enter Your Token Hear")
             print ('')
-            tok=int(input('\033[1;92m\n[*]PASTE TOKEN :\033[1;97m '))
+            token=int(input('\033[1;92m\n[*]PASTE TOKEN :\033[1;97m '))
             j=open('token.txt','w')
-            j.write(tok)
+            j.write(token)
             j.close()
             try:
                 r=requests.get('https://graph.facebook.com/me?access_token=' + tok)
